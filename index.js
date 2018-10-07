f7eca0f5be5373a6cb2bbe1a901b47e1ae3261ef
Ghazanfar Ali <ghazanfarali23@gmail.com>
2018-10-06 19:26:18 -0500

    callback route handler added

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ passport.use(
   )
 );
 
+// route handler
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+// callback route handle
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
